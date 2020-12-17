95b46ad99940b29199023d66f0683cdcbf78a4d0
Mdo <godzilla4477@gmail.com>
2020-12-18 00:05:21 +0900

    Snake 움직임 구현 및 타겟 생성

--- snake_game/main.cpp
+++ snake_game/main.cpp
@@ -1,11 +1,13 @@
 #include <Windows.h>
-#include <stdio.h>
 #include <conio.h>
-#include <vector>
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+
 #include <queue>
+#include <vector>
 
 using namespace std;
-
 
 #define LEFT 75
 #define RIGHT 77
@@ -13,107 +15,137 @@ using namespace std;
 #define DOWN 80
 #define MAX_EDGE 100
 
-void gotoxy(int x, int y)
-{
-    COORD pos = { x, y };
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+
+
+void gotoxy(int x, int y) {
+  COORD pos = {x, y};
+  SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
-class Snake
-{
-private:
-    vector<pair<int, int>> body;
+class Target {
+ public:
+  int target_cnt, x, y;
+  Target() { 
+    target_cnt = 0;
+    x = rand() % 50;
+    y = rand() % 50;
+  
+    gotoxy(x, y);
+    printf("★");
+  }
 
-public:
-    Snake()
-    {
-        body.push_back(make_pair(1, 1));
+  void CreateNewTarget(const vector<pair<int, int>>& snake_body) {
+    target_cnt++;
+    bool retry = true;
+    while (retry) {
+      x = rand() % 50;
+      y = rand() % 50;
+
+      retry = false;
+      for (auto p : snake_body) {
+        if (p.first == x && p.second == y) {
+          retry = true;
+          break;
+        }
+      }
     }
 
+    gotoxy(x, y);
+    printf("★");
+  }
+};
+
+class Snake {
+ public:
+  vector<pair<int, int>> body;
+  Snake() { body.push_back(make_pair(1, 1)); }
+
+  void draw() {
+    for (auto i : body) {
+      gotoxy(i.first, i.second);
+      printf("@");
+    }
+  }
+
+  void move(int direction) {
+    if (body.size() == 0)
+      return;
+
+    int body_len = body.size();
+
+    pair<int, int> top = body[0];
+    switch (direction) {
+      case LEFT:
+        if (top.first > 0)
+          top.first--;
+        break;
+      case RIGHT:
+        if (top.first < MAX_EDGE)
+          top.first++;
+        break;
+      case UP:
+        if (top.second > 0)
+          top.second--;
+        break;
+      case DOWN:
+        if (top.second < MAX_EDGE)
+          top.second++;
+        break;
+    }
+
+    // 음식을 먹으면
+
+    // 마지막 몸 삭제
+    gotoxy(body[body_len - 1].first, body[body_len - 1].second);
+    printf(" ");
     
-    void move(int direction)
-    {
-        if (body.size() == 0) return;
-
-        pair<int, int> top = body[0];
-        switch (direction)
-        {
-        case LEFT:
-            if(top.first > 0 ) top.first--;
-            break;
-        case RIGHT:
-            if (top.first < MAX_EDGE) top.first++;
-            break;
-        case UP:
-            if (top.second > 0) top.second--;
-            break;
-        case DOWN:
-            if (top.second < MAX_EDGE) top.second++;
-            break;
-        }
-
-        for (int i = 1; i < body.size(); i++)
-        {
-            body[i] = body[i - 1];
-        }
-        body[0] = top;
-
+    for (int i = 1; i < body_len; i++) {
+      body[i] = body[i - 1];
     }
-    void draw()
-    {
-        for (auto i : body)
-        {
-            gotoxy(i.first, i.second);
-            printf("@");
-        }
-    }
+    body[0] = top;
+
+    // 대가리 그림
+    gotoxy(body[0].first, body[0].second);
+    printf("@");
+  }
+
+  void body_inc() {
+
+  }
 };
 
 
 
-void draw_edge()
-{
-
-}
-
 bool inc_snake_len = false;
-int main()
-{
-    char input = 0;
-    Snake snake;
-    snake.draw();
-    
-    while (1)
-    {
-        if (_kbhit())
-        {
-            input = _getch();
-            if (input == -32)
-            {
-                input = _getch();
-                if (inc_snake_len == false)
-                {
-                    //gotoxy(x, y);
-                    //printf(" ");
-                }
-                else
-                {
-                    inc_snake_len = false;
-                }
-                snake.move(input);
-                snake.draw();
+int main() {
+  char input = 0;
+  srand(time(NULL));
+  
+  Target target;
+  Snake snake;
+  
 
-            }
-            else if(input == '1')
-            {
-                inc_snake_len = true;
-                //break;
-            }
-            
+  snake.draw();
+
+  while (1) {
+    if (_kbhit()) {
+      input = _getch();
+      if (input == -32) {
+        input = _getch();
+        if (inc_snake_len == false) {
+          // gotoxy(x, y);
+          // printf(" ");
+        } else {
+          inc_snake_len = false;
         }
+        snake.move(input);
+
+      } else if (input == '1') {
+        inc_snake_len = true;
+        // break;
+      }
     }
-     
+  }
 
-
-    return 0;
+  return 0;
 }
